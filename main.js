36eac56b75200f9db28767a00978e05cba8e6ed0
Rebecca <108008511+Rclarkeweb@users.noreply.github.com>
2023-07-11 15:53:37 +0100

    Updated files

--- main.js
+++ main.js
@@ -1,26 +1,49 @@
-// Generate an age between 1 and 16 at random
-let maxAge = 16;
+// Declare variables
+const guessMsg = document.getElementById("guessMsg");
+const guessContainer = document.getElementById('guessContainer');
+const playAgain = document.getElementById('playAgain');
+
+// Set number of guesses variable
+let numOfGuesses = 1;
+
+// Generate an age between 1 and 10 at random
+let maxAge = 10;
 let age = Math.floor(Math.random() * maxAge + 1);
 
-// if number entered is 0 or greater than 16 display alerts and show your guess and answer
+
 function check() {
-    let y = document.getElementById('guessTheAge').value;
-    let a;
-    if (y > 16) {
-        alert('Please use a number between 1 and 16')
-    } else if (y == 0) {
-        alert('Please enter a value')
-    } else if (y == age) {
-        document.getElementById('displayAnswer').innerText = `Correct! Well done! 
-        Your guess was ${y} and the answer was ${age}!!`;
-        document.getElementById('playAgain').style.display="inline";
-    } else if (y != age) {
-        document.getElementById('displayAnswer').innerText = `Oops, you were wrong!
-        Your guess was ${y} and the answer was ${age}!!`;
-        document.getElementById('playAgain').style.display="inline";
-    } else {
-        document.getElementById('displayAnswer').innerText = `Your guess was ${y} and the 
-        answer was ${age}!!`;
-        document.getElementById('playAgain').style.display="inline";
+    // Get the users guess value
+    const guess = document.getElementById('guessTheAge').value;
+
+    // If the users guess is equal to 0 or over the maximum age alert an error
+    if (guess == 0 || guess > maxAge) {
+        alert(`Please enter a value between one and ${maxAge}`);
+    } 
+    // 
+    else {
+        // If users guess is equal to age end game
+        if (guess == age) {
+            guessMsg.innerText = `Well done! 
+                You guessed right! The answer was ${age}! You got it right in ${numOfGuesses} guesses!`;
+                guessContainer.style.display="none";
+                playAgain.style.display="inline";
+        }
+        // If users guess is greater than the generated age
+        else if (guess > age) {
+            // Add to number of guesses
+            numOfGuesses++;
+
+            // Display msg to user
+            guessMsg.innerText = `${guess} is to high, try a smaller number`;
+        } 
+        // If users guess is smaller than generated age
+        else {
+            // Add to number of guesses
+            numOfGuesses++;
+
+            // Display msg to user
+            guessMsg.innerText = `${guess} is to small, try a larger number`;
+        }
     }
-}
+};
+
